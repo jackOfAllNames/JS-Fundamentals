7a45e40cfc3ab4a011fc4a13fb9b1b511922c185
Olaitan Kehinde <105742731+alvicci@users.noreply.github.com>
2025-05-24 22:31:36 +0100

    a script that prints a square

---
+++ 8-square.js
@@ -0,0 +1,14 @@
+const firstArgument = process.argv[2];
+const convertedFirstArgument = parseInt(firstArgument);
+
+if (!convertedFirstArgument) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < convertedFirstArgument; i++) { 
+    let square = "";  
+    for (let j = 0; j < convertedFirstArgument; j++) { 
+      square += "x";
+    }
+    console.log(square);
+  }
+}
