f5471111f648eb8faaee7f943829aec81cb4b93b
Olaitan Adedokun <kenzdex@gmail.com>
2025-05-25 18:08:17 +0100

    updated the square character from x to X

--- 8-square.js
+++ 8-square.js
@@ -4,10 +4,10 @@ const convertedFirstArgument = parseInt(firstArgument);
 if (!convertedFirstArgument) {
   console.log("Missing size");
 } else {
-  for (let i = 0; i < convertedFirstArgument; i++) { 
-    let square = "";  
-    for (let j = 0; j < convertedFirstArgument; j++) { 
-      square += "x";
+  for (let i = 0; i < convertedFirstArgument; i++) {
+    let square = "";
+    for (let j = 0; j < convertedFirstArgument; j++) {
+      square += "X";
     }
     console.log(square);
   }
